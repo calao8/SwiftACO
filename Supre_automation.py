1f6dd4c87ffd46e7129580b814e4503dadfc19fb
cajlao <70192202+calao8@users.noreply.github.com>
2020-08-26 10:14:46 -0700

    Update Supre_automation.py
    -Added Headless browser
    -Fixed Variables

--- Supre_automation.py
+++ Supre_automation.py
@@ -4,24 +4,25 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+options = Options()
+options.headless = True
+driver = webdriver.Chrome('/usr/local/bin/chromedriver', chrome_options=options)
 
 # Address information
 
 category = "t-shirts"
-name = "John Done"
-email = "123testpy@gmail.com"
-telephone = "4154923458"
-address_1 = "123 Main St"
+name = "first_last"
+email = "email_here"
+telephone = "phone number"
+address_1 = "address here"
 address_2 = ""
-zip_code = "94124"
-City = "San Francisco"
-State = "CA"
-Card_number = "4266567890123456"
-month = "04"
-year = "2025"
-CVV = "359"
+zip_code = "zipcode here"
+City = "city_name"
+State = "state name (XX)"
+card_number = "card number (xxxx_xxxx_xxxx_xxxx)"
+month = "(xx)"
+year = "(xxxx)"
+cvv = "(xxx) or (xxxx)"
 
 # auto checkout
 driver.get("https://www.supremenewyork.com/shop/t-shirts/pd75fskci/cwduzct1g")
@@ -36,10 +37,10 @@ driver.find_element_by_xpath('//*[@id="bo"]').send_keys(address_1)
 driver.find_element_by_xpath('//*[@id="order_billing_zip"]').send_keys(zip_code)
 driver.find_element_by_xpath('//*[@id="order_billing_city"]').send_keys(City)
 driver.find_element_by_xpath('//*[@id="state_label"]')
-driver.find_element_by_xpath('//*[@id="rnsnckrn"]').send_keys("4266567890123456")
-driver.find_element_by_xpath('//*[@id="credit_card_month"]').send_keys("03")
-driver.find_element_by_xpath('//*[@id="credit_card_year"]').send_keys("2025")
-driver.find_element_by_xpath('//*[@id="orcer"]').send_keys("395")
+driver.find_element_by_xpath('//*[@id="rnsnckrn"]').send_keys(card_number)
+driver.find_element_by_xpath('//*[@id="credit_card_month"]').send_keys(month)
+driver.find_element_by_xpath('//*[@id="credit_card_year"]').send_keys(year)
+driver.find_element_by_xpath('//*[@id="orcer"]').send_keys(cvv)
 driver.find_element_by_xpath('//*[@id="cart-cc"]/fieldset/p[2]/label/div/ins').click()
 time.sleep(1.5)
 driver.find_element_by_xpath('//*[@id="pay"]/input').click()
